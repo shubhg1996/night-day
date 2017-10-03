f5120563e47ef23046072f9add989bc2ce1f01d1
Shubh <shubhg1996@gmail.com>
2017-10-03 23:56:49 +0530

    tested

---
+++ midday.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/gsettings set org.gnome.desktop.interface gtk-theme 'Ant-Bloody'
+/usr/bin/gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+/usr/bin/gsettings set org.gnome.desktop.background picture-uri 'file:///home/shubh/Documents/night-day/midday.jpg'
