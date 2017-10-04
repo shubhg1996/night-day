b3945289bde24b8a41325c7005c7792677ac7931
Shubh <shubhg1996@gmail.com>
2017-10-04 15:50:52 +0530

    icon theme

--- night.sh
+++ night.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /usr/bin/gsettings set org.gnome.desktop.interface gtk-theme 'Ant-Dracula'
-/usr/bin/gsettings set org.gnome.desktop.interface icon-theme 'Papirus-dark'
+/usr/bin/gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 /usr/bin/gsettings set org.gnome.desktop.background picture-uri 'file:///home/shubh/Documents/night-day/night.jpg'
 /usr/bin/nohup /usr/bin/redshift &>/dev/null &
