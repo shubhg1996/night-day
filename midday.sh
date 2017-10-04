26c5ad5eef78a0d9592713c6033fcff85ece27e4
Shubh <shubhg1996@gmail.com>
2017-10-04 15:45:23 +0530

    redshift added

--- midday.sh
+++ midday.sh
@@ -3,3 +3,4 @@
 /usr/bin/gsettings set org.gnome.desktop.interface gtk-theme 'Ant-Bloody'
 /usr/bin/gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 /usr/bin/gsettings set org.gnome.desktop.background picture-uri 'file:///home/shubh/Documents/night-day/midday.jpg'
+/usr/bin/nohup /usr/bin/redshift &>/dev/null &
